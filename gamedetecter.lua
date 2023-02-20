0cef1020aa00f91fa8ca5d112fba9ec572ca2db1
EldmonstretPasta <89604461+EldmonstretPasta@users.noreply.github.com>
2023-02-20 13:47:19 +0100

    Create gamedetecter.lua

---
+++ gamedetecter.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 286090429 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/EldmonstretPasta/Desupo/main/arsenal.lua"), true)
+else
+	game.Players.LocalPlayer:Kick("Game not supported.")
+end
