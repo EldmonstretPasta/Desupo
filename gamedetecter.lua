b80ad4c8c514013bdda9014ef87d775a2c707f45
EldmonstretPasta <89604461+EldmonstretPasta@users.noreply.github.com>
2023-02-24 16:01:33 +0100

    Update gamedetecter.lua

--- gamedetecter.lua
+++ gamedetecter.lua
@@ -2,6 +2,4 @@ if game.PlaceId == 286090429 then
 	loadstring(game:HttpGet(("https://raw.githubusercontent.com/EldmonstretPasta/Desupo/main/arsenal.lua"), true))()
 elseif game.PlaceId == 3101667897 then
 	loadstring(game:HttpGet(("https://raw.githubusercontent.com/EldmonstretPasta/Desupo/main/legendsofspeed.lua"), true))()
-else
-	game.Players.LocalPlayer:Kick("Game not supported.")
 end
