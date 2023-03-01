212cd4b4f261448922e323720acd03626e4e0a11
EldmonstretPasta <89604461+EldmonstretPasta@users.noreply.github.com>
2023-03-01 13:12:00 +0100

    Update gamedetecter.lua

--- gamedetecter.lua
+++ gamedetecter.lua
@@ -2,4 +2,6 @@ if game.PlaceId == 286090429 then
 	loadstring(game:HttpGet(("https://raw.githubusercontent.com/EldmonstretPasta/Desupo/main/arsenal.lua"), true))()
 elseif game.PlaceId == 3101667897 then
 	loadstring(game:HttpGet(("https://raw.githubusercontent.com/EldmonstretPasta/Desupo/main/legendsofspeed.lua"), true))()
+elseif game.PlaceId == 301549746 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/EldmonstretPasta/Desupo/main/Counter%20Blox.lua"), true))()
 end
